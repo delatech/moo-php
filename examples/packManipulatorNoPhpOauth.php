19341fe5ccf02e842773b1e52f9d883a48e5c087
Jonathan Oddy <jonathan@woaf.net>
2013-11-02 12:41:26 +0000

    Added Client implementation which uses Lusitanian's OAuth client library to sign requests.
    This avoids the dependency on the PHP OAuth module.
    At present it doesn't implement 2/3-legged OAuth, but then no useful API methods require that, so *shrug*

---
+++ examples/packManipulatorNoPhpOauth.php
@@ -0,0 +1,164 @@
+<?php
+use \MooPhp\MooInterface\Data as Data;
+
+/**
+ * Demo of fiddling about with a pack on the commandline.
+ * This example does not depend on the PHP OAuth module.
+ * This will create a businesscard with an image side, and a details side.
+ * There'll be an image on both sides, and some text on the details side.
+ *
+ * @package packManipulator
+ * @author Jonathan Oddy <jonathan@moo.com>
+ * @copyright Copyright (c) 2012, Moo Print Ltd.
+ */
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// You installed this with composer, right?
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$opts = getopt("k:s:w:");
+
+if (!isset($opts["k"]) || !isset($opts['s'])) {
+    die("Need to provide key and secret.");
+}
+
+$localWeasel = getenv("USE_LOCAL_WEASEL");
+if ($localWeasel) {
+    $loader->add('Weasel', $localWeasel, true);
+}
+
+$key = $opts['k'];
+$secret = $opts['s'];
+$client = new \MooPhp\Client\Lusitanian\LusitanianDirectOauthClient($key, $secret);
+
+// This client impl doesn't support legged OAuth at the moment, so this is all you need to do.
+
+$api = new \MooPhp\Api($client);
+$weaselFactory = new \Weasel\WeaselDoctrineAnnotationDrivenFactory();
+
+if (class_exists('\Monolog\Logger')) {
+    $weaselLogger = new \Monolog\Logger("weasel");
+    $clientLogger = new \Monolog\Logger("client");
+    $apiLogger = new \Monolog\Logger("api");
+    $handler = new \Monolog\Handler\StreamHandler("php://stderr");
+    $weaselLogger->pushHandler($handler);
+    $clientLogger->pushHandler($handler);
+    $apiLogger->pushHandler($handler);
+
+    $weaselFactory->setLogger($weaselLogger);
+    $client->setLogger($clientLogger);
+    $api->setLogger($apiLogger);
+}
+
+$api->setWeaselFactory($weaselFactory);
+
+
+// Helper that allows us to calculate text sizes
+$textHelper = new \MooPhp\Helper\TextHelper($api);
+
+// First we'll create a pack, using the default physical spec (a businesscard product.)
+$packResp = $api->packCreatePack(new \MooPhp\MooInterface\Data\PhysicalSpec());
+$packId = $packResp->getPackId();
+$pack = $packResp->getPack();
+
+
+// Lets upload a photo
+$uploadImageResp = $api->imageUploadImage(__DIR__ . '/poor_kettle_purchasing_decision.jpg');
+$basketItem = $uploadImageResp->getImageBasketItem();
+
+// And add it to our pack's imagebasket so we can use it on our cards.
+$pack->getImageBasket()->addItem($basketItem);
+
+// Lets make an image side
+$imageTemplateCode = "businesscard_full_image_landscape";
+
+$imageSide = new Data\Side();
+$imageSide->setType("image")->setTemplateCode($imageTemplateCode);
+
+/**
+ * @var \MooPhp\MooInterface\Data\Template\Items\ImageItem $imageTemplateItem
+ */
+// We need to work out how to position our image. The TemplateItem for the image allows us to calculate a sane default.
+$imageTemplate = $api->templateGetTemplate($imageTemplateCode);
+$imageTemplateItem = $imageTemplate->getItemByLinkId("variable_image_front");
+$printImage = $basketItem->getImageItem("print");
+$imageBox = $imageTemplateItem->calculateDefaultImageBox($printImage->getWidth(), $printImage->getHeight());
+
+$imageData = new Data\UserData\ImageData();
+$imageData->setLinkId("variable_image_front");
+$imageData->setImageBox($imageBox);
+$imageData->setResourceUri($basketItem->getResourceUri());
+
+$pack->addSide($imageSide->addDatum($imageData));
+
+// That's an image side done, move onto the details side.
+
+$detailsTemplateCode = "businesscard_right_image_landscape";
+
+$detailsSide = new Data\Side();
+$detailsSide->setType("details")->setTemplateCode($detailsTemplateCode);
+
+// OK, lets put the same image we've already used on the details side.
+$imageData = new Data\UserData\ImageData();
+$imageData->setLinkId("variable_image_back");
+
+// You may wonder where these numbers came from... well I had to use the Moo flash canvas to make it look right,
+// and then pull the data out of the pack data that wrote.
+$imageData->setImageBox(new Data\Types\BoundingBox(new Data\Types\Point(78.09, 29.85), 81.81, 61.36));
+$imageData->setResourceUri($basketItem->getResourceUri());
+$detailsSide->addDatum($imageData);
+
+// We're going to need the template loaded to make use of the TextHelper
+$detailsTemplate = $api->templateGetTemplate($detailsTemplateCode);
+
+// We'll add some text too:
+$textLine = new Data\UserData\TextData("back_line_1");
+$textLine->setFont(new Data\Types\Font("radio"))->setText("Kettles should not burn this well;");
+$textLine->setColour(new Data\Types\ColourRGB(255, 0, 0));
+
+// Rather than specifying our own font size, we'll let the text helper fit the text for us.
+$textHelper->fitTextData($textLine, $detailsTemplate);
+
+$detailsSide->addDatum($textLine);
+
+// More text, this time with a hardcoded font size
+$textLine = new Data\UserData\TextData("back_line_2");
+$textLine->setFont(new Data\Types\Font("meta"));
+$textLine->setText("Especially if they turn themselves on;");
+$textLine->setPointSize(2.65);
+$textLine->setColour(new Data\Types\ColourRGB(0, 255, 0));
+$detailsSide->addDatum($textLine);
+
+$textLine = new Data\UserData\TextData();
+$textLine->setFont(new Data\Types\Font("vagrounded"));
+$textLine->setText("This was a poor purchasing decision;");
+$textLine->setPointSize(2.65);
+$textLine->setColour(new Data\Types\ColourRGB(0, 0, 255));
+$textLine->setLinkId("back_line_3");
+$detailsSide->addDatum($textLine);
+
+$textLine = new Data\UserData\TextData();
+$textLine->setFont(new Data\Types\Font("bryant"));
+$textLine->setText("And now I cannot make tea.");
+$textLine->setPointSize(3.35);
+$textLine->setLinkId("back_line_4");
+$detailsSide->addDatum($textLine);
+
+$updateResp = $api->packUpdatePack($packId, $pack->addSide($detailsSide));
+
+// OK, all done.
+
+// We might have some warnings (though at time of writing this pack was fine.)
+var_dump($updateResp->getWarnings());
+
+// Let's get a rendering of the details side to see what it'll look like.
+$renderedUrl = $api->packRenderSideUrl($detailsSide, $pack->getImageBasket());
+var_dump($renderedUrl);
+
+// We should also be able to enter the Moo build flow at various points identified by dropins.
+var_dump($updateResp->getDropIns());
+
+print "\nAll done!\n";
+
---
+++ lib/MooPhp/Client/Lusitanian/LusitanianDirectOauthClient.php
@@ -0,0 +1,218 @@
+<?php
+namespace MooPhp\Client\Lusitanian;
+use MooPhp\Client\Client;
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Http\Uri\Uri;
+use OAuth\OAuth1\Signature\Signature;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use InvalidArgumentException;
+
+/**
+ * @package MooPhp
+ * @author Jonathan Oddy <jonathan@moo.com>
+ * @copyright Copyright (c) 2012, Moo Print Ltd.
+ *
+ *
+ * Client using the OAuth Signature code from Lusitanian's OAuth library.
+ * This does not implement 3 or 2 legged OAuth, only direct, so is not suitable
+ * for methods that require proper OAuth.
+ * At present pretty much nothing you'll want to use requires 2/3 legged OAuth.
+ * If that changes then I might stop being lazy and fix this.
+ *
+ */
+class LusitanianDirectOauthClient implements Client, LoggerAwareInterface
+{
+    public $_useOpenSSLIfAvailable = true;
+
+    protected $_ch;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $_logger;
+
+    /**
+     * @var \OAuth\OAuth1\Signature\Signature
+     */
+    protected $_signature;
+
+    /**
+     * @var \OAuth\Common\Consumer\Credentials
+     */
+    protected $_credentials;
+
+    protected $_endpoint = 'https://secure.moo.com/api/service/';
+
+    protected $_signatureMethod = "HMAC-SHA1";
+
+    /**
+     * @var \OAuth\Common\Http\Uri\Uri
+     */
+    protected $_uri;
+
+    public function __construct($apiKey, $apiSecret, LoggerInterface $logger = null)
+    {
+        $this->_credentials = new Credentials($apiKey, $apiSecret, "oob"); // Lie about oob, we don't need callback.
+        $this->_signature = new Signature($this->_credentials);
+        $this->_signature->setHashingAlgorithm($this->_signatureMethod);
+        $this->_uri = new Uri($this->_endpoint);
+
+        if (isset($logger)) {
+            $this->setLogger($logger);
+        }
+
+        $this->_ch = curl_init();
+        curl_setopt($this->_ch, CURLOPT_SSL_VERIFYPEER, false); // Why do we need to do this?!
+    }
+
+    protected function generateNonce()
+    {
+        $rawLength = 32;
+        if (function_exists('openssl_random_pseudo_bytes') && $this->_useOpenSSLIfAvailable) {
+            $byteStr = openssl_random_pseudo_bytes($rawLength);
+        } else {
+            $byteStr = '';
+            for ($i = 0; $i < $rawLength; $i++) {
+                $byteStr .= pack('C', mt_rand(0, 256));
+            }
+        }
+        return base64_encode($byteStr);
+    }
+
+    protected function _generateAuthHeader($params, $method)
+    {
+        $oauthParams = array(
+            'oauth_consumer_key' => $this->_credentials->getConsumerId(),
+            'oauth_signature_method' => $this->_signatureMethod,
+            'oauth_timestamp' => date('U'),
+            'oauth_nonce' => $this->generateNonce(),
+            'oauth_version' => '1.0'
+        );
+
+        $signature = $this->_signature->getSignature($this->_uri, array_merge($oauthParams, $params), $method);
+
+        $oauthParams['oauth_signature'] = $signature;
+
+        $authHeaderParts = array();
+        foreach ($oauthParams as $key => $value) {
+            $authHeaderParts[] = rawurlencode($key) . '="' . rawurlencode($value) . '"';
+        }
+        return 'OAuth ' . implode(',', $authHeaderParts);
+    }
+
+    /**
+     * @param array $params
+     * @param string $method
+     * @throws \Exception|\OAuthException
+     * @throws \InvalidArgumentException
+     * @return string
+     */
+    public function makeRequest(array $params, $method = self::HTTP_POST)
+    {
+
+        if ($this->_logger) {
+            $this->_logger->debug("Making Request", array("params" => $params));
+        }
+
+
+        $ch = $this->_ch;
+
+        $url = $this->_uri->getAbsoluteUri();
+
+        $authHeader = $this->_generateAuthHeader($params, $method);
+
+        $paramStr = http_build_query($params);
+
+        switch ($method) {
+            case self::HTTP_GET:
+                curl_setopt($ch, CURLOPT_HTTPGET, true);
+                $url .= '?' . $paramStr;
+                break;
+            case self::HTTP_POST:
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $paramStr);
+                break;
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                "Authorization: " . $authHeader,
+                "Expect:"
+            ));
+
+        return $this->_doCurl();
+    }
+
+    private function _doCurl()
+    {
+        $rawResponse = curl_exec($this->_ch);
+        if ($this->_logger) {
+            $this->_logger->debug("Got Response", array("rawResponse" => $rawResponse));
+        }
+        $errno = curl_errno($this->_ch);
+        if (!$rawResponse || $errno != CURLE_OK) {
+            throw new \RuntimeException("Error making request: ($errno) " . curl_error($this->_ch));
+        }
+        return $rawResponse;
+    }
+
+    /**
+     * @param array $params
+     * @param string $fileParam The param that contains the path on disk to the file
+     * @throws \RuntimeException
+     * @return mixed
+     */
+    public function sendFile(array $params, $fileParam)
+    {
+        $ch = $this->_ch;
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $this->_endpoint);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Expect:"));
+
+        $params[$fileParam] = '@' . $params[$fileParam];
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+
+        if ($this->_logger) {
+            $this->_logger->debug("Making Request", array("params" => $params));
+        }
+        return $this->_doCurl();
+    }
+
+    public function getFile(array $params)
+    {
+        $ch = $this->_ch;
+        curl_setopt($ch, CURLOPT_HTTPGET, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Expect:"));
+
+        $url = $this->_endpoint . '?' . http_build_query($params);
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        if ($this->_logger) {
+            $this->_logger->debug("Making Request", array("params" => $params));
+        }
+        return $this->_doCurl();
+
+    }
+
+    public function getLogger()
+    {
+        return $this->_logger;
+    }
+
+    /**
+     * Sets a logger instance on the object
+     *
+     * @param LoggerInterface $logger
+     * @return null
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+    }
+}
